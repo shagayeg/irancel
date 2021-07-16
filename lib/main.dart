4b910f79a2f5b3221786745d112d9fa0879885b0
shagayeg <shagayegheydarzade@gmail.com>
2021-07-16 18:32:16 +0430

    finish in the part grid view

--- lib/main.dart
+++ lib/main.dart
@@ -268,111 +268,269 @@ class _MyHomePageState extends State<MyHomePage>
               height: MediaQuery.of(context).size.height * 0.50,
               width: MediaQuery.of(context).size.width * 0.97,
               child: GridView.count(
-                 physics: BouncingScrollPhysics(), 
-                crossAxisCount: 3, children: [
-                GridTile(
-                  child: Card(
-                   
-                    child:
-                      Icon(Icons.home),
-                      
-                    
-                  ),
-                ),
-                GridTile(
-                  child: Card(
-                   
-                    child:
-                      Icon(Icons.home),
-                      
-                    
-                  ),
-                ),
-                GridTile(
-                  child:  Card(
-                  
-                    child:
-                      Icon(Icons.home),
-                      
-                    
-                  ),
-                ),
-                GridTile(
-                  child: Card(
-                   
-                    child:
-                      Icon(Icons.home),
-                      
-                    
-                  ),
-                ),
-                GridTile(
-                  child: Card(
-                   
-                    child:
-                      Icon(Icons.home),
-                      
-                    
-                  ),
-                ),
-                GridTile(
-                  child:  Card(
-                  
-                    child:
-                      Icon(Icons.home),
-                      
-                    
-                  ),
-                ),
-                GridTile(
-                  child:  Card(
-                   
-                    child:
-                      Icon(Icons.home),
-                      
-                    
-                  ),
-                ),
-                GridTile(
-                  child: Card(
-                   
-                    child:
-                      Icon(Icons.home),
-                      
-                    
-                  ),
-                ),
-                GridTile(
-                  child:  Card(
-                   
-                    child:
-                      Icon(Icons.home),
-                      
-                    
-                  ),
-                ),
-
-                // color: Colors.red,
-                //   child:  GridView.count(
-
-                // crossAxisCount: 3,
-                // childAspectRatio: 1.0,
-                // padding: const EdgeInsets.all(4.0),
-                // mainAxisSpacing: 4.0,
-                // crossAxisSpacing: 4.0,
-                // children: [
-                //   Icon(Icons.circle_notifications),
-                //   Icon(Icons.circle_notifications),
-                //   Icon(Icons.circle_notifications),
-                //   Icon(Icons.circle_notifications),
-                //   Icon(Icons.circle_notifications),
-                // ],
-                // )
-              ]))
+                  physics: BouncingScrollPhysics(),
+                  crossAxisCount: 3,
+                  children: [
+                    GridTile(
+                      child: Card(
+                          child: Stack(
+                        children: [
+                          Container(
+                              margin: EdgeInsets.only(top: 15, left: 25),
+                              height: MediaQuery.of(context).size.height * 0.08,
+                              // width: 40,
+                              // color: Colors.red,
+                              child: Image.network(
+                                  "https://assets.myket.ir/icons/xlarge/com.likotv_5ba603fe-0095-46c7-a01c-84d565989d36.png")),
+                          Positioned(
+                              top: 88,
+                              left: 60,
+                              child: Text("لنز",
+                                  style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.w700)))
+                        ],
+                      )),
+                    ),
+                    GridTile(
+                      child: Card(
+                          child: Stack(
+                        children: [
+                          Container(
+                              margin: EdgeInsets.only(top: 28, left: 25),
+                              height: MediaQuery.of(context).size.height * 0.08,
+                              // width: 40,
+                              // color: Colors.red,
+                              child: Image.network(
+                                  "https://snapp.ir/blog/wp-content/uploads/2020/09/11-opt-min.jpg")),
+                          Positioned(
+                              top: 88,
+                              left: 55,
+                              child: Text("اسنپ",
+                                  style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.w700))),
+                          Positioned(
+                              left: 70,
+                              child: Container(
+                                  padding: EdgeInsets.only(left: 8),
+                                  child: Text(
+                                    "جدید",
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                  height: 26,
+                                  width: 50,
+                                  decoration: BoxDecoration(
+                                      color: Colors.red,
+                                      borderRadius: BorderRadius.only(
+                                        bottomLeft: Radius.circular(10.0),
+                                        bottomRight: Radius.circular(10.0),
+                                      ))))
+                        ],
+                      )),
+                    ),
+                    GridTile(
+                      child: Card(
+                          child: Stack(
+                        children: [
+                          Container(
+                              margin: EdgeInsets.only(top: 28, left: 25),
+                              height: MediaQuery.of(context).size.height * 0.08,
+                              // width: 40,
+                              // color: Colors.red,
+                              child: Image.network(
+                                  "https://images.khabaronline.ir/images/2013/7/front1.jpg")),
+                          Positioned(
+                              top: 88,
+                              left: 55,
+                              child: Text("بیمه عمر",
+                                  style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.w700))),
+                        ],
+                      )),
+                    ),
+                    GridTile(
+                      child: Card(
+                          child: Stack(
+                        children: [
+                          Container(
+                              margin: EdgeInsets.only(top: 15, left: 25),
+                              height: MediaQuery.of(context).size.height * 0.08,
+                              // width: 40,
+                              // color: Colors.red,
+                              child: Image.network(
+                                  "https://play-lh.googleusercontent.com/j-hOa6Ze_X5URuSeQGaCzNUIY7J4f59gv74LDsIU7WaEC7GjBz3SkBZggveMv2UPsHg")),
+                          Positioned(
+                              top: 88,
+                              left: 10,
+                              child: Text("پیشواز آهنگ پیشواز",
+                                  style: TextStyle(
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.w700))),
+                          Positioned(
+                              left: 70,
+                              child: Container(
+                                  padding: EdgeInsets.only(left: 8),
+                                  child: Text(
+                                    "جدید",
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                  height: 26,
+                                  width: 50,
+                                  decoration: BoxDecoration(
+                                      color: Colors.red,
+                                      borderRadius: BorderRadius.only(
+                                        bottomLeft: Radius.circular(10.0),
+                                        bottomRight: Radius.circular(10.0),
+                                      )))),
+                        ],
+                      )),
+                    ),
+                    GridTile(
+                      child: Card(
+                          child: Stack(
+                        children: [
+                          Positioned(
+                            left: 90,
+                            child: CustomPaint(
+                              painter: Chevron(),
+                              child: Container(
+                                width: 20.0,
+                                height: 30.0,
+                                child: Padding(
+                                  padding: EdgeInsets.only(top: 30.0),
+                                  child: Align(
+                                    alignment: Alignment.topCenter,
+                                    child: Text("1",
+                                        style: TextStyle(fontSize: 24.0)),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      )),
+                    ),
+                     
+                     GridTile(
+                      child: Card(
+                          child: Stack(
+                        children: [
+                          Container(
+                              margin: EdgeInsets.only(top: 15, left: 25),
+                              height: MediaQuery.of(context).size.height * 0.08,
+                              // width: 40,
+                              // color: Colors.red,
+                              child: Image.network(
+                                  "http://storage2.avvalmarket.ir/apps/com.zoodfood.android.play/icon/512x512.png")),
+                          Positioned(
+                              top: 88,
+                              left: 30,
+                              child: Text("اسنپ فود",
+                                  style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.w700)))
+                        ],
+                      )),
+                    ),
+                     
+                    GridTile(
+                      child: Card(
+                          child: Stack(
+                        children: [
+                          Container(
+                              margin: EdgeInsets.only(top: 15, left: 40),
+                              height: MediaQuery.of(context).size.height * 0.08,
+                              // width: 40,
+                              // color: Colors.red,
+                              child: Text("...",style: TextStyle(fontSize: 40,fontWeight: FontWeight.w800),)
+                                  ),
+                          Positioned(
+                              top: 88,
+                              left: 30,
+                              child: Text("بیشتر",
+                                  style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.w700)))
+                        ],
+                      )),
+                    ),
+                    GridTile(
+                      child: Card(
+                          child: Stack(
+                        children: [
+                          Container(
+                              margin: EdgeInsets.only(top: 15, left: 25),
+                              height: MediaQuery.of(context).size.height * 0.08,
+                              // width: 40,
+                              // color: Colors.red,
+                              child: Image.network(
+                                  "https://tenovin.com/wp-content/uploads/2019/10/orderfoodonline.png")),
+                          Positioned(
+                              top: 88,
+                              left: 30,
+                              child: Text("اسنپ مارکت",
+                                  style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.w700)))
+                        ],
+                      )),
+                    ),
+                     GridTile(
+                      child: Card(
+                          child: Stack(
+                        children: [
+                          Container(
+                              margin: EdgeInsets.only(top: 15, left: 25),
+                              height: MediaQuery.of(context).size.height * 0.08,
+                              // width: 40,
+                              // color: Colors.red,
+                              child: Image.network(
+                                  "https://lh3.googleusercontent.com/proxy/pOPrL40RlIEFaCOFX-Zt9uer2gcYXILBHXo9ZNahfQ5VBCkkGOppgU8EqLUZaDZzx08_hmpRtmJiL-WpwtjBpFMpnXOl0j698j0EmvPYtg")),
+                          Positioned(
+                              top: 88,
+                              left: 30,
+                              child: Text("همراه هوشمند",
+                                  style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.w700)))
+                        ],
+                      )),
+                    ),
+                  ]))
         ])));
   }
 }
 
+class Chevron extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final Gradient gradient = new LinearGradient(
+      begin: Alignment.topCenter,
+      end: Alignment.bottomCenter,
+      colors: [Colors.orangeAccent, Colors.red],
+      tileMode: TileMode.clamp,
+    );
+
+    final Rect colorBounds = Rect.fromLTRB(0, 0, size.width, size.height);
+    final Paint paint = new Paint()
+      ..shader = gradient.createShader(colorBounds);
+
+    Path path = Path();
+    path.moveTo(0, 0);
+    path.lineTo(0, size.height);
+    path.lineTo(size.width / 2, size.height - size.height / 3);
+    path.lineTo(size.width, size.height);
+    path.lineTo(size.width, 0);
+    path.close();
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => false;
+}
 // void main() {
 //   runApp(MyApp());
 // }
